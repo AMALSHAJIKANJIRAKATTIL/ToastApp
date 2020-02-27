1cdb87a72b3d7f4d0f1a7b84b337b0798d63f83a
Amalshajikanjirakattil <amalshajikmofficial@gmail.com>
2020-02-27 21:43:31 +0530

    Toast app First Try

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(this,"Me Too!" ,Toast.LENGTH_SHORT).show()
+        }
+    }
+}
